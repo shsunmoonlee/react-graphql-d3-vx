18ad39c9eb0eb2439a945ee037c363fbc66d4456
Seunghun Lee <markandsh@gmail.com>
2018-12-14 12:33:44 +0100

    heroku deployment set up

---
+++ build/precache-manifest.78c6204c911f9fe5e449f0731fd5f7d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6e2eda92d7171fb80254",
+    "url": "/static/js/main.6e2eda92.chunk.js"
+  },
+  {
+    "revision": "914741ee80c2b95bd339",
+    "url": "/static/js/1.914741ee.chunk.js"
+  },
+  {
+    "revision": "6e2eda92d7171fb80254",
+    "url": "/static/css/main.8d37e369.chunk.css"
+  },
+  {
+    "revision": "914741ee80c2b95bd339",
+    "url": "/static/css/1.29b29740.chunk.css"
+  },
+  {
+    "revision": "055d2c18f633d01d696a9c8c29f4aca0",
+    "url": "/index.html"
+  }
+];
